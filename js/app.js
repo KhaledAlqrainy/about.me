2785286fb3b4737b5f6914e08fe49b8e17234745
KhaledAlqrainy <khaledalqrainy@gmail.com>
2021-05-25 23:49:09 +0300

    add q6

--- js/app.js
+++ js/app.js
@@ -1,103 +1,53 @@
 'use strict';
 
-let sportinfo = prompt ('are you into sports?')
-sportinfo = sportinfo.toLowerCase();
-console.log(sportinfo);
+let points =0;
+let q5 = prompt('PSG won the Champions leauge last year');
+
+switch (q5.toLowerCase()) {
+    case 'yes':
+    case 'y':
+        //console.log(true)
+        alert('oops')
+        break;
+
+    case 'no':
+    case 'n':
+        //console.log(false)
+        alert('good')
+        points++;
+        break;
 
 
-if(sportinfo ==='yes' || sportinfo ==='y')
-{
-    alert ('good for you');
 }
-else if(sportinfo ==='no' || sportinfo === 'n') {
-    alert ('you have to !')
-}
-else {
-    alert('you have to pick between yes or no');
-}
+
+let picknum = prompt('how many times did AC Milan won the champions leauge?');
+
+    for ( var x=0 ; x >4 ; x++)
+    {
+        if (picknum === 7) {
+            alert ('Great !');
+            points++;
+            
+            
+
+        }
+
+        else if (picknum >7 ){
+            picknum = prompt(' too high try again');
+        }
+
+        else if (picknum<7) {
+            picknum = prompt(' too low, try again');
+        }
+        
+    }
 
 
-alert ('answer the following questions with yes or no');
- let q1 = prompt ('have Ajax team ever won the champion leauge?');
-
- switch (q1.toLowerCase())
- {
-     case 'yes':
-     //console.log(true)
-     alert ('Good');
-     break;
-
-     case 'no':
-      //console.log(false)
-     alert ('oops');
-     break;
 
 
- }
 
- let q2 = prompt ('Germany team won the world cup 3 times');
 
- switch (q2.toLowerCase())
- {
-     case 'yes':
-      //console.log(true)
-     alert ('oops');
-     break;
 
-     case 'no':
-     //console.log(false)
-     alert ('good');
-     break;
-    
-     
 
- }
- let q3 = prompt ('Kaka won the golden ball two times');
 
- switch (q3.toLowerCase())
- {
-     case 'yes':
-      //console.log(true)
-     alert ('oops');
-     break;
-
-     case 'no':
-     //console.log(false)
-     alert ('good');
-     break;
-     
-
- }
- let q4 = prompt ('Roger Federer have 20 Grand slam')
- switch (q4.toLowerCase())
- {
-     case 'yes':
-      //console.log(true)
-     alert ('Good');
-
-     case 'no':
-     //console.log(false)
-     alert ('oops');
-     break;
-     
-
- }
- let q5 = prompt ('PSG won the Champions leauge last year');
-
- switch (q5.toLowerCase())
- {
-     case 'yes':
-      //console.log(true)
-     alert('oops')
-     break;
-
-     case 'no':
-     //console.log(false)
-     alert('good')
-    break;
-     
-
- }
-
- alert ('You did great !');
 
